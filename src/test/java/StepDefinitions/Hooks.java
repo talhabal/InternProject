67f0dcc82e0ad287abbba816622b211dfb7040a2
talhabal <68268759+talhabal@users.noreply.github.com>
2023-04-07 22:23:59 +0300

    Temel fonksiyonlar oluşturuldu.

---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,15 @@
+package StepDefinitions;
+
+import Utilities.GeneralWebDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    @After
+    public void after(){
+        System.out.println("Senaryo bitti!");
+        GeneralWebDriver.quitDriver();
+    }
+}
