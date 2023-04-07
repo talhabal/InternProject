e3c3956c529aea2519739509806bf79d43eeb122
talhabal <68268759+talhabal@users.noreply.github.com>
2023-04-07 22:00:12 +0300

    General Web Driver tamamlandı.

--- src/test/java/Utilities/DriverControl.java
+++
@@ -1,8 +0,0 @@
-package Utilities;
-
-public class DriverControl {
-    public static void main(String[] args){
-        System.out.println("Selam group4!");
-        System.out.println("Umarım süpersinizdir!");
-    }
-}
---
+++ src/test/java/Utilities/GeneralWebDriver.java
@@ -0,0 +1,43 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GeneralWebDriver {
+    private static WebDriver driver;
+    public static WebDriver getDriver(){
+        // Türkçe rapor oluşturulurken tüm bilgisayarlarda çalışabilmesi için aşağıdaki kodu ekliyoruz
+        Locale.setDefault(new Locale("EN"));
+        System.setProperty("user.language", "EN");
+
+        // Console'da bulunan Chrome hatalarını gizlemek için
+        Logger logger= Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+
+        // Eğer driver yoksa direkt olarak Chrome ile açılıyor
+        if (driver==null){
+            driver  =   new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void quitDriver(){
+        // 4000 milisaniye sonra program kapanmazsa otomatik olarak kapatılır
+        try {
+            Thread.sleep(4000);
+        }
+        catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        if (driver!=null){
+            driver.quit();
+            driver      =   null;
+        }
+    }
+}
