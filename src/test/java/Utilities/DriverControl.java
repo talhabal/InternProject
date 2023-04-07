3ca0501ac99e091209392719ef1219eac7b51574
talhabal <68268759+talhabal@users.noreply.github.com>
2023-04-07 18:16:51 +0300

    First Updated

--- src/test/java/Utilities/DriverControl.java
+++ src/test/java/Utilities/DriverControl.java
@@ -3,5 +3,6 @@ package Utilities;
 public class DriverControl {
     public void write(){
         System.out.println("Selam group4!");
+        System.out.println("Umarım süpersinizdir!");
     }
 }
