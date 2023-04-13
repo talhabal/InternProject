e125ce908db8b38ca7fa7e199eb2d3c3beb25e42
talhabal <68268759+talhabal@users.noreply.github.com>
2023-04-13 23:57:09 +0300

    Nationalities create-edit-delete

--- src/test/java/Pages/LeftNav.java
+++ src/test/java/Pages/LeftNav.java
@@ -2,6 +2,7 @@ package Pages;
 
 import Utilities.GeneralWebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LeftNav extends Parent{
@@ -9,12 +10,24 @@ public class LeftNav extends Parent{
         PageFactory.initElements(GeneralWebDriver.getDriver(),this);
     }
 
+    @FindBy(xpath = "(//span[text()='Setup'])[1]")
+    public WebElement setup;
+    @FindBy(xpath = "//span[text()='Parameters']")
+    public WebElement parameters;
+    @FindBy(xpath = "//span[text()='Nationalities']")
+    public WebElement nationalities;
+
+    @FindBy(xpath = "//*[text()='Bank Accounts']")
+    public WebElement bankButton;
 
     public WebElement getWebElement(String strButton){
         switch (strButton)
         {
-            //case "setup" : return setup;
-            //case "parameters" : return parameters;
+            case "setup" : return setup;
+            case "parameters" : return parameters;
+            case "nationalities" : return nationalities;
+            case "bankAccount": return bankButton;
+
         }
         return null;
     }
