ed0021a3ac7d415cc1e25100151a49872ac22ed4
talhabal <68268759+talhabal@users.noreply.github.com>
2023-04-20 15:13:45 +0300

    In the School Departments add,edit,delete and verify were tested

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -56,17 +56,7 @@ public class Parent {
         new Actions(GeneralWebDriver.getDriver()).sendKeys(Keys.ESCAPE).perform(); //açık dialog varsa kapatmak için esc yapıyoruz
     }
     // Aldığımız uyarı hataları ile ilgili işlem yapar.
-    public void undefinedContainsTextFunction(WebElement element,String value){
-        wait.until(ExpectedConditions.textToBePresentInElement(element,value));
-        Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()));
-        new Actions(GeneralWebDriver.getDriver()).sendKeys(Keys.ESCAPE).perform(); //açık dialog varsa kapatmak için esc yapıyoruz
-    }
 
-    public void undefinedTablesTextFunction(WebElement element,String value){
-        wait.until(ExpectedConditions.textToBePresentInElement(element,value));
-        Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()));
-        new Actions(GeneralWebDriver.getDriver()).sendKeys(Keys.ESCAPE).perform(); //açık dialog varsa kapatmak için esc yapıyoruz
-    }
 
     public void undefinedContainsTextFunction(WebElement element,String value){
         wait.until(ExpectedConditions.textToBePresentInElement(element,value));
--- src/test/java/StepDefinitions/INTERN_8.java
+++ src/test/java/StepDefinitions/INTERN_8.java
@@ -1,12 +1,44 @@
 package StepDefinitions;
 
 import Pages.DialogContent;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class INTERN_8 {
     DialogContent dialogContent     =   new DialogContent();
     @Then("Verify that the operation was unsuccessful")
     public void verifyThatTheOperationWasUnsuccessful() {
       dialogContent.undefinedContainsTextFunction(dialogContent.errorMessageContent, "already");
+    }
+
+
+    @And("Click on the element on the screen")
+    public void clickOnTheElementOnTheScreen(DataTable dt) {
+        List<String> strButtons=dt.asList(String.class);
+        for (String strButton: strButtons) {
+            WebElement element=dialogContent.getWebElement(strButton); // webelemnti bul
+            dialogContent.clickFunction(element); // webelemente tıklat
+        }
+    }
+
+    @And("Enter a value for the input on the screen")
+    public void enterAValueForTheInputOnTheScreen(DataTable dt) {
+        List< List<String> > items= dt.asLists(String.class);
+        for (int i = 0; i < items.size(); i++) {
+            WebElement element= dialogContent.getWebElement(items.get(i).get(0));
+            dialogContent.sendKeysFunction(element, items.get(i).get(1));
+        }
+    }
+
+    @And("Find at pages and click")
+    public void findAtPagesAndClick(DataTable dt) {
+        List<String> strButtons=dt.asList(String.class);
+        for (String strText: strButtons) {
+            dialogContent.findPagesAndClick(strText);
+        }
     }
 }
